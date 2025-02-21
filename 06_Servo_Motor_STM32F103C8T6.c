2bc71a971ec35dc4b1f4cf05b28ad7e11a2b302d
Kağan Şahin <161020917+KaganxSahin@users.noreply.github.com>
2025-02-21 13:43:17 +0300

    Update 06_Servo_Motor_STM32F103C8T6.c

--- 06_Servo_Motor_STM32F103C8T6.c
+++ 06_Servo_Motor_STM32F103C8T6.c
@@ -16,6 +16,21 @@ Prescaler set to 15
 Counter Period 9999
 */
 
+//Wiring
+/*
+- **STM32 Microcontroller**:
+  - The `A1` pin is connected to the PWM signal pin of the SG90 Servo Motor.
+  - The `5V` pin is connected to the power input of the servo motor.
+  - The `G` (GND) pin is connected to the ground terminal of the servo motor.
+
+- **SG90 Servo Motor**:
+  - The red wire (`VCC`) is connected to the `5V` pin of the STM32.
+  - The brown wire (`GND`) is connected to the `GND` pin of the STM32.
+  - The orange wire (`PWM` signal) is connected to the `A1` pin of the STM32.
+
+These connections allow the STM32 to control the angle of the servo motor by generating PWM signals.
+*/
+
 //Additional code on top of STM32CubeIDE generated code
  /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
@@ -36,18 +51,3 @@ Counter Period 9999
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
-
-//Wiring
-/*
-- **STM32 Microcontroller**:
-  - The `A1` pin is connected to the PWM signal pin of the SG90 Servo Motor.
-  - The `5V` pin is connected to the power input of the servo motor.
-  - The `G` (GND) pin is connected to the ground terminal of the servo motor.
-
-- **SG90 Servo Motor**:
-  - The red wire (`VCC`) is connected to the `5V` pin of the STM32.
-  - The brown wire (`GND`) is connected to the `GND` pin of the STM32.
-  - The orange wire (`PWM` signal) is connected to the `A1` pin of the STM32.
-
-These connections allow the STM32 to control the angle of the servo motor by generating PWM signals.
-*/
