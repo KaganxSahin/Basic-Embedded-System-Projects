f54e2f6d0c05113e87f8f9e16021773794d24628
Kağan Şahin <161020917+KaganxSahin@users.noreply.github.com>
2025-02-21 13:41:23 +0300

    Update 02_LED_Fade_STM32F103C8T6.c

--- 02_LED_Fade_STM32F103C8T6.c
+++ 02_LED_Fade_STM32F103C8T6.c
@@ -16,6 +16,22 @@ Prescaler set to 127
 Counter Period 625
 */
 
+//Wiring
+/*
+- **STM32 Microcontroller**:
+  - The G pin is connected to the negative (ground) rail of the breadboard.
+  - The A1 pin is connected to the positive terminal of a current-limiting resistor.
+  - The other end of the resistor is connected to the anode (positive terminal) of the LED.
+  - The cathode (negative terminal) of the LED is connected to the negative rail of the breadboard.
+
+- **Breadboard Connections**:
+  - The negative rail (ground) is connected to the G pin of the STM32.
+  - The positive terminal of the current-limiting resistor is connected to the A1 pin of the STM32.
+  - The cathode (negative terminal) of the LED is connected to the negative rail (ground) of the breadboard.
+
+Resistor value can be anywhere between 100R - 330R.
+*/
+
 //Additional code on top of STM32CubeIDE generated code
 /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
@@ -42,19 +58,3 @@ Counter Period 625
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
-
-//Wiring
-/*
-- **STM32 Microcontroller**:
-  - The G pin is connected to the negative (ground) rail of the breadboard.
-  - The A1 pin is connected to the positive terminal of a current-limiting resistor.
-  - The other end of the resistor is connected to the anode (positive terminal) of the LED.
-  - The cathode (negative terminal) of the LED is connected to the negative rail of the breadboard.
-
-- **Breadboard Connections**:
-  - The negative rail (ground) is connected to the G pin of the STM32.
-  - The positive terminal of the current-limiting resistor is connected to the A1 pin of the STM32.
-  - The cathode (negative terminal) of the LED is connected to the negative rail (ground) of the breadboard.
-
-Resistor value can be anywhere between 100R - 330R.
-*/
